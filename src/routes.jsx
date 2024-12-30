d8b25dd2fe983597d5b31e4ce14777b83faef572
JunSeong <gentlmanjun@naver.com>
2024-12-30 15:33:12 +0900

    feat: set router

---
+++ src/routes.jsx
@@ -0,0 +1,62 @@
+import CardNewsError from "./components/common/CardNewsError";
+import MainLayout from "./components/layouts/MainLayout";
+import MainIndex from "./pages/main/index";
+import CreateIndex from "./pages/create/index";
+import DownloadIndex from "./pages/download/index";
+import PrivateRoute from "./components/common/PrivateRoute";
+
+const AccessType = Object.freeze({
+  PUBLIC: "PUBLIC", // Everyone can access (Default)
+  PRIVATE: "PRIVATE", // Authorized user can access only
+});
+
+const cardNewsRoutes = [
+  {
+    path: "",
+    accessType: AccessType.PUBLIC,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "",
+        accessType: AccessType.PUBLIC,
+        element: <MainIndex />,
+      },
+      {
+        path: "create",
+        accessType: AccessType.PUBLIC,
+        element: <CreateIndex />,
+      },
+      {
+        path: "download",
+        accessType: AccessType.PUBLIC,
+        element: <DownloadIndex />,
+      },
+    ],
+  },
+];
+
+const injectProtectedRoute = (routes) => {
+  const injectProtectedComp = (route) => {
+    if (route.accessType === AccessType.PRIVATE) {
+      route.element = <PrivateRoute>{route.element}</PrivateRoute>;
+    }
+
+    return route;
+  };
+
+  return routes.map((route) => {
+    route = injectProtectedComp(route);
+
+    if (route?.children) {
+      route.children = route.children.map((route) =>
+        injectProtectedComp(route)
+      );
+    }
+
+    route.errorElement = <CardNewsError />;
+
+    return route;
+  });
+};
+
+export const routes = injectProtectedRoute(cardNewsRoutes);
