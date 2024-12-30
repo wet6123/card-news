14c76650b93db36d6e38d56b4dce3ecde2c4c7c4
JunSeong <gentlmanjun@naver.com>
2024-12-30 15:32:52 +0900

    init: init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { routes } from "./routes";
+
+const router = createBrowserRouter(routes);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
